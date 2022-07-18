b621943bacebb3ca0d8254832ebf18ecc2648f23
DongHyun Yun <yun980722@naver.com>
2022-07-19 02:35:40 +0900

    3장 완료

--- src/main/java/com/example/springbootwebservice/domain/posts/Posts.java
+++ src/main/java/com/example/springbootwebservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.example.springbootwebservice.domain.posts;
 
+import com.example.springbootwebservice.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter                 //클래스 내 모든 필드의 Getter 메소드를 자동생성
 @NoArgsConstructor      //기본 생성자 추가 == public posts
 @Entity         // 테이블과 링크 될 클래스임을 나타낸다.
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id         // 해당 테이블의 PK 필드를 나타낸다.
     @GeneratedValue(strategy = GenerationType.IDENTITY) //  PK의 생성 규칙을 나타낸다.
@@ -28,5 +29,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
     }
 }
